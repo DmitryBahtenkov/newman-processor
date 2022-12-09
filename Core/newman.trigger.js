636ede04673f42437ae887598f41fea5d5334be0
Dmitry Bahtenkov <d.bahtenkov@skillaz.ru>
2022-12-09 10:39:20 +0300

    add message limit

--- Core/newman.trigger.js
+++ Core/newman.trigger.js
@@ -2,8 +2,11 @@ const newman = require('../Core/newman.api')
 const postmanApi = require('../Core/postman.api')
 const sendMessage = require('../Core/telegram.api')
 
+const MESSAGE_LENGTH = 4096;
+
 const executeCollection = async (postmanKey, data) => {
     const collections = await postmanApi.getAllCollections(postmanKey, data.collections);
+    console.log(collections);
     for (const collectionId of collections) {
         await newman(`https://api.getpostman.com/collections/${collectionId}?apikey=${postmanKey}`, (error, summary) => {
             if(summary) {
@@ -20,17 +23,22 @@ const executeCollection = async (postmanKey, data) => {
                     + `Всего проверок: ${totalTests}\n`
                     + `Тестов с ошибкой: ${failTests}\n`;
 
+                let parameters = '';
+
                 if(failTests) {
-                    message += `***\n${fails.join('\n')}`
+                    message += `***\n${fails.map(x => `- ${x}`).join('\n')}`
                 }
 
                 if(data.params) {
-                    message += '\n***'
+                    parameters += '\n***'
                     for (const key of Object.keys(data.params)) {
-                        message += `\n${key}: ${data.params[key]}`
+                        parameters += `\n${key}: ${data.params[key]}`
                     }
                 }
 
+                if(message.length >= MESSAGE_LENGTH) {
+                    message = message.substring(0, MESSAGE_LENGTH - parameters.length) + parameters;
+                }
 
                 sendMessage(message)
                     .then(r => console.log('Telegram sending ok'))
